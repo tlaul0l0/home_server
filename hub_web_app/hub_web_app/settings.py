fc5b35f779bc8958aeff9cca02f17f00a1b1bdf6
tlaul0l0 <tim.laukemann@gmx.de>
2024-01-03 15:41:31 +0100

    remove key

--- hub_web_app/hub_web_app/settings.py
+++ hub_web_app/hub_web_app/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6p(#^prlr$d*i59#m1s)x!lke8)$fqx=eizefrno=64z$nkrgd'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
